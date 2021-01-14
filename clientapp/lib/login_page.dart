2392effb7acfc3c72fd8adc17bfb4ba7369cd8e7
Tanya Agarwal <tanyaagarwal0012@gmail.com>
2021-01-14 21:27:34 +0530

    login pg updated

--- clientapp/lib/login_page.dart
+++ clientapp/lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginPage extends StatefulWidget {
@@ -17,130 +18,139 @@ class _LoginPageState extends State<LoginPage> {
           gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
-              stops: [0.3, 1],
-              colors: [Color(0xff564E6D), Colors.black]),
+              colors: [Color(0xff00032D), Color(0xff000000),Color(0xff05001D)]),
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+
           children: [
             Padding(
               padding: const EdgeInsets.only(top: 60.0),
               child: Text(
                 'PORTAL',
-                style: TextStyle(
-                  fontFamily: 'Raleway',
-                  fontWeight: FontWeight.w700,
+                style: GoogleFonts.raleway(
+
+                  fontWeight: FontWeight.w500,
                   color: Color(0xffF2F2F2),
-                  fontSize: 41.0,
+                  fontSize: 45.0,
                 ),
+
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(top: 50.0),
-              child: ClipOval(
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: RadialGradient(
-                        colors: [Color(0xff564E6D), Color(0x40564E6D)]),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color(0XFF272531),
-                        spreadRadius: 10,
-                        blurRadius: 5,
-                        offset: Offset(0, 7),
-                      ),
-                    ],
+              padding: EdgeInsets.only(top: 100.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Welcome !',
+                    style: GoogleFonts.raleway(
+                      color: Color(0xffE4FCF9),
+                      fontSize: 30,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  height: 400.0,
-                  width: 350.0,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(top: 20.0),
-                        child: Text(
-                          'Welcome !',
-                          style: TextStyle(
-                            color: Color(0xffE4FCF9),
-                            fontSize: 30,
-                            fontFamily: 'Raleway',
+
+                  Padding(padding: EdgeInsets.only(top:90.0),
+                    child: Container(
+                      height: 50,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        color: Color(0xffD33535),
+                        borderRadius:BorderRadius.all(Radius.circular(25.0)),
+
+                      ),
+
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 5.0,right: 15.0),
+                            child: Icon(FontAwesomeIcons.googlePlusG,
+                                size: 30.0,
+                                color: Colors.white,
+                            ),
                           ),
-                          textAlign: TextAlign.center,
-                        ),
+                          Text(
+                            'Sign in with Google',
+                            style: GoogleFonts.raleway(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+
+                          ),
+
+                        ],
                       ),
-                      SizedBox(
-                        height: 60.0,
+                    ),
+                  ),
+
+
+                  Padding(padding: EdgeInsets.only(top:70.0),
+                    child: Container(
+                      height: 50,
+                      width: 300,
+
+                      decoration: BoxDecoration(
+                        color: Colors.black54,
+                        borderRadius:BorderRadius.all(Radius.circular(25.0)),
+                      border: Border.all(
+                        color: Colors.white,
+                      )),
+
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only( right: 15.0),
+                            child: Icon(FontAwesomeIcons.github,
+                              size: 30.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            'Sign in with Github',
+                            style: GoogleFonts.raleway(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+
+                        ],
                       ),
-                      Container(
-                        height: 50.0,
-                        width: 300,
-                        child: ListTile(
-                          onTap: () {},
-                          leading: Icon(
-                            FontAwesomeIcons.googlePlusG,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(top:70.0),
+                  child: Container(
+                    height: 50,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: Color(0xff4C4B99),
+                      borderRadius:BorderRadius.all(Radius.circular(25.0)
+                    ),),
+
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding:  EdgeInsets.only(left:10.0,right: 15.0),
+                          child: Icon(FontAwesomeIcons.facebookSquare,
                             size: 30.0,
                             color: Colors.white,
                           ),
-                          tileColor: Color(0xffD33535),
-                          title: Text(
-                            'Sign in with Google',
-                            style: TextStyle(
-                              color: Color(0xffFFFFFF),
+                        ),
+                        Text(
+                          'Sign in with Facebook',
+                            style: GoogleFonts.raleway(
                               fontSize: 20,
-                              fontFamily: 'Raleway',
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 20.0),
-                        child: Container(
-                          height: 50,
-                          width: 300,
-                          child: ListTile(
-                            onTap: () {},
-                            leading: Icon(
-                              FontAwesomeIcons.facebookSquare,
                               color: Colors.white,
-                              size: 30.0,
                             ),
-                            tileColor: Color(0xff4C4B99),
-                            title: Text(
-                              'Sign in with Facebook',
-                              style: TextStyle(
-                                color: Color(0xffFFFFFF),
-                                fontSize: 20,
-                                fontFamily: 'Raleway',
-                              ),
-                            ),
-                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20.0),
-                        child: Container(
-                          height: 50,
-                          width: 300,
-                          child: ListTile(
-                            onTap: () {},
-                            leading: Icon(
-                              FontAwesomeIcons.github,
-                              color: Colors.white,
-                              size: 30.0,
-                            ),
-                            tileColor: Colors.black54,
-                            title: Text(
-                              'Sign in with Github',
-                              style: TextStyle(
-                                color: Color(0xffFFFFFF),
-                                fontSize: 20,
-                                fontFamily: 'Raleway',
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+
+                      ],
+                    ),
                   ),
-                ),
+                  ),
+                ],
               ),
             ),
           ],
