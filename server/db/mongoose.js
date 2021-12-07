3d0c2cbcde9d17629f4217171fdcc9baa1fc537e
Ujjwal Agarwal <56391382+specter25@users.noreply.github.com>
2021-12-07 14:42:19 +0530

    Revert "Created Routes for Login, Register, View Profile and Logout"

--- server/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-
-mongoose.connect("mongodb+srv://prabs3257:manhaton@cluster0.fepe1.mongodb.net/acm-portal?retryWrites=true&w=majority", (err) => {
-  
-    if (err) throw err;
-    console.log("Connected to MongoDB");
-});
--- server/middleware/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(req, res, next) {
-    if(req.session.isAuth){
-        next()
-    }else{
-        res.status(401).send("Unauthenticated")
-    }
-};
