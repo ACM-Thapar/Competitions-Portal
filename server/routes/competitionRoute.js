688bd24b5db8c76ef916fbaacb657f8fee606de5
Aviral-tech <nagpalavi2001@gmail.com>
2022-01-27 23:20:14 +0530

    added filter for competitions

--- server/routes/competitionRoute.js
+++ server/routes/competitionRoute.js
@@ -13,4 +13,44 @@ router.get('/getcompetitions', async (req, res) => {
   }
 });
 
+router.post('/filter', async (req, res) => {
+  if (req.body.filter == 'team_members') {
+    const team_size = req.body.team_size;
+    if (team_size == '1') {
+      const competitions = await Competition.find({
+        teamSize: 'Team Size Individual Participation ',
+      });
+      console.log(competitions);
+      res.status(200).json({ success: true, data: competitions });
+    } else if (team_size == '2') {
+      const competitions = await Competition.find({
+        teamSize: 'Team Size 2 Members ',
+      });
+      res.status(200).json({ success: true, data: competitions });
+    } else {
+      const competitions = await Competition.find({
+        teamSize: 'Team Size 1 - 4 Members ',
+      });
+      res.status(200).json({ success: true, data: competitions });
+    }
+  } else if (req.body.filter == 'fees') {
+    const fees = req.body.fees;
+    if (fees == 'paid') {
+      const competitions = await Competition.find({
+        $nor: [{ fees: ' Registration Fee  Free ' }, { fees: 'Free' }],
+      });
+      res.status(200).json({ success: true, data: competitions });
+    } else {
+      const competitions = await Competition.find({
+        $or: [
+          { fees: ' Registration Fee  Free ' },
+          { fees: 'Free' },
+          { fees: '' },
+        ],
+      });
+      res.status(200).json({ success: true, data: competitions });
+    }
+  }
+});
+
 module.exports = router;
