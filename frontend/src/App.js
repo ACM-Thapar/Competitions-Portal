085a4eea96246a009fbc2ae2686ffc92f0068b6a
paarth-sharma <paarthsharma2003@gmail.com>
2022-07-16 20:37:30 +0530

    started work on frontend using react.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginPage from './LoginPage';
+
+function App() {
+  return (
+    <div>
+      <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
